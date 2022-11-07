09be5267cef910fd1a600cacdd89df5752cd7419
Vishal <vishaldas6566@gmail.com>
2022-11-08 02:27:43 +0530

    added

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -7,28 +7,39 @@ import TimeAndLocation from './components/TimeAndLocation';
 import TemperatureAndDetails from './components/TemperatureAndDetails';
 import Forecast from './components/Forecast';
 import getFormattedWeatherData from './services/weatherService';
+import { useEffect, useState } from 'react';
 
 
 
 function App() {
 
+const [query,setQuery]=useState({q:'berlin'})
+const [units,setUnits]=useState("metric")
+const [weather,setWeather]=useState(null)
 
-  
-  const fetch=async()=>{
-const data=await getFormattedWeatherData({q:"london"})
-console.log(data)
-  }
-  fetch()
+useEffect(()=>{
+  const fetchWeather=async()=>{
+    const data=await getFormattedWeatherData({...query,units}).then((data)=>(
+      setWeather(data)
+    ))
+    // console.log(data)
+      }
+      fetchWeather()
+},[query,units])
   return (
   <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400'>
     <TopButtons />
     <Inputs />
 
-    
-    <TimeAndLocation />
-    <TemperatureAndDetails />
+    {weather && (
+      <div>
+      <TimeAndLocation weather={weather}/>
+    <TemperatureAndDetails  weather={weather} />
     <Forecast title='hourly forecast' />
     <Forecast title='daily forecast' />
+    </div>
+    )}
+    
 
   </div>
   )
--- weather-app/src/components/TemperatureAndDetails.jsx
+++ weather-app/src/components/TemperatureAndDetails.jsx
@@ -1,44 +1,47 @@
 import React from 'react'
 import { UilArrowUp, UilArrowDown, UilTemperature, UilTear, UilWind, UilSun, UilSunset } from "@iconscout/react-unicons"
-const TemperatureAndDetails = () => {
+import { formatToLocalTime, iconUrlFromCode } from '../services/weatherService'
+const TemperatureAndDetails = ({weather:{
+    details,icon,temp,temp_min,temp_max,sunrise,sunset,humidity,speed,feels_like,timezone
+}}) => {
     return (
         <div>
             <div className='flex  items-center justify-center py-6 text-xl text-cyan-300'>
-                <p>cloudy or whatever</p>
+                <p>{details}</p>
             </div>
             <div className='flex flex-row items-center justify-between text-white py-3'>
-                <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="" className='w-20' />
-                <p className='text-5xl'>34°</p>
+                <img src={iconUrlFromCode(icon)} alt="" className='w-20' />
+                <p className='text-5xl'>{`${temp.toFixed()}°`}</p>
                 <div className='flex flex-col space-y-2'>
                     <div className='flex font-light text-sm items-center justify-center'>
                         <UilTemperature size={18} className='mr-1' />
-                        Real Feel: <span className='font-md ml-1'>34°</span>
+                        Real Feel: <span className='font-md ml-1'>{`${feels_like.toFixed()}°`}</span>
                     </div>
                     <div className='flex font-light text-sm items-center justify-center'>
                         <UilTear size={18} className='mr-1' />
-                        Humidity: <span className='font-md ml-1'>65%</span>
+                        Humidity: <span className='font-md ml-1'>{`${humidity.toFixed()}%`}</span>
                     </div>
                     <div className='flex font-light text-sm items-center justify-center'>
                         <UilWind size={18} className='mr-1' />
-                      Wind: <span className='font-md ml-1'>11 km/hr</span>
+                      Wind: <span className='font-md ml-1'>{`${speed.toFixed()}km/hr`}</span>
                     </div>
                 </div>
             </div>
             <div className='flex flex-row items-center justify-center space-x-2 text-white text-sm py-3'>
                 <UilSun />
-                <p className='font-light'>Rise:<span className='font-medium ml-1'>06:45 AM</span></p>
+                <p className='font-light'>Rise:<span className='font-medium ml-1'>{formatToLocalTime(sunrise,timezone,"hh:mm a")}</span></p>
                 <p  className='font-light'>|</p>
 
                 <UilSunset />
-                <p className='font-light'>Set:<span className='font-medium ml-1'>06:45 PM</span></p>
+                <p className='font-light'>Set:<span className='font-medium ml-1'>{formatToLocalTime(sunset,timezone,"hh:mm a")}</span></p>
                 <p  className='font-light'>|</p>
 
                 <UilSun />
-                <p className='font-light'>High:<span className='font-medium ml-1'>45°</span></p>
+                <p className='font-light'>High:{" "}<span className='font-medium ml-1'>{`${temp_max.toFixed()}°`}</span></p>
                 <p  className='font-light'>|</p>
 
                 <UilSun />
-                <p className='font-light'>Low:<span className='font-medium ml-1'> 45°</span></p>
+                <p className='font-light'>Low:{" "}<span className='font-medium ml-1'>{`${temp_min.toFixed()}°`}</span></p>
                 
             </div>
         </div>
